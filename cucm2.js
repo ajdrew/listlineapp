b0f886c1ebe6da5966479d956ff9e4240f8f70e1
ajdrew <ajdrew@outlook.com>
2017-10-24 12:32:16 -0400

    Default browser pop + Mac/Win/Lin apps

--- cucm2.js
+++ cucm2.js
@@ -6,12 +6,13 @@ const bodyParser = require("body-parser")
 const hbs = require('hbs')
 var xml2js = require('xml2js');
 var parser = new xml2js.Parser();
+var opn = require('opn');
 
 // APP - DEFINITION
 const app = express()
 
 // APP - BUILD
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.engine('html', require('hbs').__express);
@@ -19,22 +20,17 @@ app.set('view engine', 'html');
 
 // EXPRESS ROUTE - INDEX
 app.get('/', function (req, res) {
-  res.render(path.join(__dirname+ '/views/index.html'), {
-    'title': 'CUCM 2.0'
-  });
-})
-
-// // EXPRESS ROUTE - RESULTS
-app.get('/cucmmapper-results', function (req, res) {
-  res.sendFile(path.join(__dirname+ '/public/cucm2-results.html'), {
-    'title': 'CUCM 2.0'
+  res.render('index.html', {
+    title: 'CUCM 2.0'
   });
 })
 
 // EXPRESS ROUTING - INCLUDE - CUCM MAPPER
-var routingextensions = require(__dirname+ '/routes/cucmmapper.js')(app);
+var routingextensions = require('./routes/cucmmapper.js')(app);
 
 // APP - START
 app.listen(3000, function () {
-  console.log('CUCM 2.0 listening on port 3000!')
-})
+  console.log('Please keep this terminal open until finished as it is running the backend API communications with CUCM.')
+});
+
+opn('http://localhost:3000');
